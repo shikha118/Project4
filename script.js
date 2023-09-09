58544890df3e9ec02969d6fff8014889b463c70f
shikha118 <110533202+shikha118@users.noreply.github.com>
2023-09-09 22:46:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let string=   "";
+let buttons= document.querySelectorAll('.button');
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener('click',(e)=>{
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            document.querySelector('input').value = string;
+        }
+        else{
+
+            console.log(e.target)  
+            string= string + e.target.innerHTML;
+            document.querySelector('input').value = string;
+        }
+        if(e.target.innerHTML == 'RESET'){
+            string ="";
+            document.querySelector('input').value = string;
+        }
+        if(e.target.innerHTML == 'DEL'){
+            string= string.substring(0,string.length-4)   ;
+            document.querySelector('input').value = string;
+        }
+        
+    })
+})
